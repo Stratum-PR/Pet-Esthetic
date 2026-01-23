dce9f38db0c19fbddb9d5b1a895f21ead5001a49
Jovaniel Rodriguez <58681@icf.com>
2026-01-23 17:02:26 -0400

    Updated the splash page timesheet updates script to use the new format_normalized_datetime function.

--- scripts/Noloco_Splash_Page_Timesheet_Updates.py
+++ scripts/Noloco_Splash_Page_Timesheet_Updates.py
@@ -1024,7 +1024,7 @@ def main():
         
         # Check if current time is 9 AM in Puerto Rico (allows 9:00-9:59 AM window)
         now_pr = datetime.now(ZoneInfo('America/Puerto_Rico'))
-        is_email_hour = now_pr.hour == 16
+        is_email_hour = now_pr.hour == 17
         
         # Print email decision
         print("\n" + "=" * 80)
